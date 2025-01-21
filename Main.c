1cd584354717cdf31071e9cbf282f082424fc55a
Kaizu-r <jljoloc@gmail.com>
2025-01-22 00:32:36 +0800

    ADD: ray traced shadows (can be buggy)

--- Main.c
+++ Main.c
@@ -6,8 +6,8 @@
 #include "shader.h"
 #include "utils.h"
 
-#define WIDTH 60
-#define HEIGHT 30
+#define WIDTH 80
+#define HEIGHT 40
 #define FPS 12
 
 int main(){
@@ -37,8 +37,10 @@ int main(){
         4,5,1,
         2,3,6,
         6,7,3,
-       // 0,2,4,
-        //4,2,6,
+        0,2,4,
+        4,2,6,
+        1,3,5,
+        5,3,7
     };
 
     vec3 vertices2[] = {
@@ -60,7 +62,7 @@ int main(){
         -0.5, -0.5, -0.5, 0,
         0.5, 0.5, -0.5, 0,
         0.5, -0.5, -0.5, 0,
-
+        //back
         -0.5, 0.5, 0.5, 0,
         -0.5, -0.5, 0.5, 0,
         0.5, 0.5, 0.5, 0,
@@ -75,8 +77,10 @@ int main(){
         4,5,1,
         2,3,6,
         6,7,3,
-        //0,2,4,
-        //4,2,6,
+        0,2,4,
+        4,2,6,
+        1,3,5,
+        5,3,7
     };
     //left to right in terms of x
     int indices2[] = {
@@ -88,8 +92,10 @@ int main(){
         4,5,1,
         2,3,6,
         6,7,3,
-        //0,2,4,
-        //4,2,6,
+        0,2,4,
+        4,2,6,
+        1,3,5,
+        5,3,7
     };
 
     int indCount1 = sizeof(indices)/sizeof(int);
@@ -100,25 +106,26 @@ int main(){
     int comb_ind[indCount1 + indCount2];
     int total_ind[ind_res];
     //setup rotation here
-    vec3 model_rotation = {10,10,0};
+    vec3 model_rotation = {0,10,0, 0};
 
     //set up initial rotation
-    vec3 model_init_rotation = {0, 0, 0};
+    vec3 model_init_rotation = {0, 0, 0, 0};
 
 
     //setup translation here
-    vec3 model_translation = {20, 0, -10};
+    vec3 model_translation = {5, 0, 0, 0};
 
     //light
-    vec3 light = {0, -10, 0};
+    vec3 light = {-WIDTH/2, HEIGHT * 2, 0, 0};
+    //light = toTerminal(&light, WIDTH, HEIGHT);
 
     //setup camera here
-    float far = 70;
+    float far = 50;
     float near = 0.2;
 
-    vec3 camera_trans = {0, 0, 0};
+    vec3 camera_trans = {0, 0, 0, 0};
     //camera rotation
-    vec3 camera_rot = {0,0,0};
+    vec3 camera_rot = {0,0,0, 0};
     //focal lenght. greater values will zoom it in
     float focal = 1;
 
@@ -126,9 +133,9 @@ int main(){
     float fov= 90;
 
 
-    vec3 view_rotation = {0, 0, 0};
-    vec3 view_translation = {0, 0, 0};
-    vec3 total_rotation = {0,0,0};
+    vec3 view_rotation = {0, 0, 0, 0};
+    vec3 view_translation = {0, 0, 0, 0};
+    vec3 total_rotation = {0,0,0, 0};
     int n = sizeof(vertices)/sizeof(vec3);
     int m = sizeof(vertices2)/sizeof(vec3);
     int g = sizeof(ground)/sizeof(vec3);
@@ -148,10 +155,10 @@ int main(){
     //initialize the screen
     char screen[HEIGHT * WIDTH + HEIGHT];
     int point_len = HEIGHT * WIDTH * 1.5;
-    vec3 *points;
+    //vec3 *points;
     while(1){
 
-
+        
         //copy vertex data to modvert
         for(int i = 0; i < n; i++){
             modVert[i] = vertices[i];
@@ -196,13 +203,15 @@ int main(){
                 
         //rotate initially, then rotate based on the incrementing rotation
 
-        modelTransform(modVert, n, 12, (vec3) {total_rotation.x + model_init_rotation.x, total_rotation.y + model_init_rotation.y, total_rotation.z + model_init_rotation.z},  model_translation);
-        modelTransform(modVert2, m, 12, (vec3) {total_rotation.x + model_init_rotation.x, -(total_rotation.y + model_init_rotation.y), total_rotation.z + model_init_rotation.z},  (vec3) {-10, -5, 10});
-        modelTransform(groundVert, g,12, (vec3) {0, total_rotation.y + model_init_rotation.y, total_rotation.z + model_init_rotation.z},  (vec3) {0, 25, -10});
+        modelTransform(modVert, n, 15, (vec3) {0, 10, 0, 0},  model_translation);
+        modelTransform(modVert2, m, 15, (vec3) {0, 0, -10, 0},  (vec3) {-10, -5, 10, 0});
+        modelTransform(groundVert, g, 15, (vec3) {0, 0, 0, 0},  (vec3) {0, -10, 0, 0});
 
 
         merge_models(modVert, modVert2, comb, indices, indices2, comb_ind, n, m, indCount1, indCount2);
         merge_models( comb, groundVert, viewM, comb_ind, ground_ind, total_ind, n + m, g, indCount1 + indCount2, indCount3);
+
+        modelTransform(viewM, n + g + m, 1, total_rotation,  (vec3) {0, 0, 0, 0});
 
         view(viewM, n + m + g, view_translation, view_rotation);
         for(int i = 0; i < n + m + g; i++){
@@ -228,9 +237,9 @@ int main(){
         //number of points per line total
         //int point_len = pointsLen(terminal, indices, 3, sizeof(indices)/sizeof(int));
         //temporary scale our point
-
         //see if our modified lines are working properly
-        points = malloc(point_len * sizeof(vec3));
+        vec3 points[point_len * 2];
+        
         int offset = 0;
         int stride = 3;
         int shapes = (sizeof(total_ind)/sizeof(int))/stride;
@@ -239,27 +248,43 @@ int main(){
             //emit_light(&terminal[indices[offset]], &terminal[indices[offset + 1]], &terminal[indices[offset + 2]], light, WIDTH, HEIGHT);
             makeShape(terminal, total_ind, points, stride, i, &offset);
         }
-        //proj(points, point_len, far, near, fov, WIDTH, HEIGHT);
-        int final_points_len = zBuffer(points, offset + 1, WIDTH, HEIGHT);
+        vec3 points_copy[offset + 1];
+        for(int i = 0; i <= offset; i++){
+            points_copy[i] = points[i];
+        }
+        //proj(points_copy, point_len, far, near, fov, WIDTH, HEIGHT);
+        int final_points_len = zBuffer(points_copy, offset + 1, WIDTH, HEIGHT);
         vec3 final_points[final_points_len];
         //transfer our data
         for(int i = 0; i < final_points_len; i++){
-            final_points[i] = points[i];
-            final_points[i].l = final_points[i].z * 1/(far - near) - (near)/(far - near);
+            final_points[i] = points_copy[i];
+            //final_points[i].l = final_points[i].z * 1/(far - near) - (near)/(far - near);
 
             //printf("%f %f %f\n", final_points[i].x, final_points[i].y, final_points[i].z);
         }
 
 
-
+        
         //attempt to render
 
         //render(final_points, final_points_len, WIDTH, HEIGHT, screen);
+        
+        emit_light(final_points, final_points_len, points, point_len, light, 0.1, WIDTH, HEIGHT);
+        for(int i = 0; i < final_points_len; i++){
+            final_points[i].l += 0.5 * (final_points[i].z * 1/(far - near) - (near)/(far - near));
+
+            //printf("%f %f %f\n", final_points[i].x, final_points[i].y, final_points[i].z);
+        }
+        //emit_light(final_points, final_points_len, points, point_len, light, 1, WIDTH, HEIGHT);
         resetFrag(frag, WIDTH, HEIGHT);
+        fragTest(frag, WIDTH, HEIGHT);
         setFrag(final_points, final_points_len, frag, WIDTH, HEIGHT);
         renderFrag(frag, WIDTH, HEIGHT, screen);
         wait(FPS);
-        clear();
+        //break;
+        system("cls");
+        //free(points);
+        //points = NULL;
         //free(screen);
 
 
