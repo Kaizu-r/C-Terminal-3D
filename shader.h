c9b793102a19c3a3bdc39a5c3b0f03ea8015828d
Kaizu-r <jljoloc@gmail.com>
2025-02-02 16:57:38 +0800

    RM: Removed old rasterizing technique ADD: Triangle primitives based rasterization

--- shader.h
+++ shader.h
@@ -9,7 +9,7 @@
 
 
 //draw line in terms of x (bresenham algo)
-void lineLow(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
+void lineLow(vec3 vert1, vec3 vert2, float **frag, int n, int WIDTH, int HEIGHT){
     int dx = vert2.x - vert1.x;
     int dy = vert2.y - vert1.y;
     int dz = vert2.z - vert1.z;
@@ -34,10 +34,9 @@ void lineLow(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
     int y = vert1.y;
     int z = vert1.z;
     for(int i = 0; i <= n; i++){
-        points[i + offset].x = i + vert1.x;
-        points[i + offset].y = y;
-        points[i + offset].z = z;
-        points[i + offset].l = vert1.l;
+        if(y >= 0 && i + (int) vert1.x >= 0
+        && y < HEIGHT && i + (int)vert1.x < WIDTH)
+            frag[i + (int) vert1.x][y] = vert1.l;
         if(D > 0){
             y += yi;
             D += (dy - dx) << 1;
@@ -54,7 +53,7 @@ void lineLow(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
     }
 }
 
-void lineHigh(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
+void lineHigh(vec3 vert1, vec3 vert2, float **frag, int n, int WIDTH, int HEIGHT){
     int dx = vert2.x - vert1.x;
     int dy = vert2.y - vert1.y;
     int dz = vert2.z - vert1.z;
@@ -89,10 +88,9 @@ void lineHigh(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
 
     //loop in reverse so it is sorted in ascending order
     for(int i = 0; i <= n; i++){
-        points[i + offset].x = x;
-        points[i + offset].y = i + vert1.y;
-        points[i + offset].z = z;
-        points[i + offset].l = vert1.l;
+        if(x >= 0 && i + (int) vert1.y >= 0
+        && x < WIDTH && i + (int) vert1.y < HEIGHT)
+            frag[x][i + (int) vert1.y] = vert1.l;
         if(D > 0){
             x += xi;
             D += (dx - dy) << 1;
@@ -110,7 +108,7 @@ void lineHigh(vec3 vert1, vec3 vert2, vec3 points[], int n, int offset){
 
 }
 
-void lineDraw(vec3 vert1, vec3 vert2, vec3 points[], int offset){
+void lineDraw(vec3 vert1, vec3 vert2, float **frag, int WIDTH, int HEIGHT){
     int x0 = vert1.x;
     int y0 = vert1.y;
     int x1 = vert2.x;
@@ -119,57 +117,23 @@ void lineDraw(vec3 vert1, vec3 vert2, vec3 points[], int offset){
     int m = abs(y0 - y1);
     if(m < n){
         if(x0 > x1)
-            lineLow(vert2, vert1, points, n, offset);
+            lineLow(vert2, vert1, frag, n, WIDTH, HEIGHT);
         else
-            lineLow(vert1, vert2, points, n, offset);
+            lineLow(vert1, vert2, frag, n, WIDTH, HEIGHT);
     }
     else{
         if(y0 > y1)
-            lineHigh(vert2, vert1, points, m, offset);
+            lineHigh(vert2, vert1, frag, m, WIDTH, HEIGHT);
         else
-            lineHigh(vert1, vert2, points, m, offset);
+            lineHigh(vert1, vert2, frag, m, WIDTH, HEIGHT);
     }
 }
 
-int lineLen(vec3 vert1, vec3 vert2){
-    int m = abs(vert1.x - vert2.x);
-    int n = abs(vert1.y - vert2.y);
-    int len = (m < n) ? n : m;
-    return len + 1;
 
-}
-
-int shapeLen(vec3 vert[], int indices[], int stride, int offset){
-    int vert_index = stride * offset;
-    int shape_len = 0;
-    //area of a our triangle
-    int a = lineLen(vert[indices[vert_index]], vert[indices[vert_index + 1]]);
-    int b = lineLen(vert[indices[vert_index + 1]], vert[indices[vert_index + 2]]);
-    int c = lineLen(vert[indices[vert_index]], vert[indices[vert_index + 2]]);
-
-    float s = (a + b + c)/2;
-
-
-
-    //big approx to give headroom
-    shape_len = sqrt((s * (s - a) * (s - b) * (s - c))) * 2;
-    return shape_len;
-}
-
-int pointsLen(vec3 vert[], int indices[], int stride, int index_size){
-    int points_len = 0;
-    int shapes_n = index_size/stride;
-    for(int i = 0; i < shapes_n; i++){
-        points_len += shapeLen(vert, indices, stride, i);
-    }
-    printf("%d\n", points_len);
-    return points_len;
-}
-
-//ray traced shadows
+//no longer usable
 void emit_light(vec3 points[], int point_len, vec3 world[], int world_len, vec3 light, float res, int distance, int WIDTH, int HEIGHT){
 
-    light = toTerminal(&light, WIDTH, HEIGHT);
+    light = toTerminal(light, WIDTH, HEIGHT);
     sortVert(world, world_len);
     int a =0, b = 0, c = 0;
     int screen_points[WIDTH][HEIGHT]; //index of points seen in the screen
@@ -243,75 +207,76 @@ void emit_light(vec3 points[], int point_len, vec3 world[], int world_len, vec3 
 
 }
 
-void raster(vec3 points[], int start, int end, int *point_offset){
-    mergesort(points, start, end); //sort first
-    for(int i = start; i <= end; i++){
-        if(points[i].y == points[i + 1].y){
-            lineDraw(points[i], points[i + 1], points, *point_offset);
-            *point_offset += lineLen(points[i], points[i + 1]);
 
-        }
 
-    }
-}
 
-//not working rip
-void makeGround(vec3 points[], int point_len, vec3 scene[], int scene_points, int HEIGHT, int WIDTH, int FAR){
-    int i = 0;
-    int j = 0;
-    vec3 curr = {10, HEIGHT/2, FAR};
+void drawTriangle(tri tri1, float **frag, int WIDTH, int HEIGHT){
     
-    mergesort(points, 0, point_len - 1);
-    while(points[i].y <= HEIGHT/2)
-        scene[j++] = points[i++];
-    while(i < point_len){
-        if(points[i].x != curr.x && points[i].y != curr.y && points[i].z != curr.z){
-            scene[j++] = curr;
-        }
-        else
-            scene[j++] = points[i];
-        if(curr.x >= WIDTH){
-            curr.y--;
-            curr.z--;
-            curr.x = 10;
-        }
-        else{
-            curr.x++;
-        }
-        i++;
-    }
+    lineDraw(tri1.v1, tri1.v2, frag, WIDTH, HEIGHT);
+    lineDraw(tri1.v2, tri1.v3, frag, WIDTH, HEIGHT);
+    lineDraw(tri1.v3, tri1.v1, frag, WIDTH, HEIGHT);
 
-    //mergesort(scene, 0, scene_points - 1);
 }
 
+void fillTriangle(tri tri1, float **frag, int WIDTH, int HEIGHT){
+    //sort points 
+    vec3 p0, p1, p2;
+    p0 = tri1.v1;
+    p1 = tri1.v2;
+    p2 = tri1.v3;
 
-void makeShape(vec3 vert[], int indices[], vec3 points[], int stride, int offset, int* point_offset){
-    int indices_start = offset * stride;
-    int index1;
-    int index2;
-    int start = *(point_offset);
-    int line_offset = *(point_offset); //starting index of starting point of a line
-    for(int i = 0; i < stride - 1; i++){
-        index1 = indices[indices_start + i];
-        index2 = indices[indices_start + i + 1];
-        
-        lineDraw(vert[index1], vert[index2], points, line_offset);
-        line_offset += lineLen(vert[index1], vert[index2]);
+    if(p1.y < p0.y)
+        swap(&p1, &p0);
+    if(p2.y < p0.y)
+        swap(&p2, &p0);
+    if(p2.y < p1.y)
+        swap(&p2, &p1);
+
+    //
+    int m, n, l;
+    m = abs(p0.y - p1.y);
+    n = abs(p1.y - p2.y);
+    l = abs(p0.y - p2.y);
+
+    int x01[m];
+    int x12[n];
+    int x02[l];
+
+    //interpolate the points
+    interpolate(x01, m, p0.y, p0.x, p1.y, p1.x);
+    interpolate(x12, n, p1.y, p1.x, p2.y, p2.x);
+    interpolate(x02, l, p0.y, p0.x, p2.y, p2.x);
+
+    m--;    //remove last point
+    int o = m + n;
+    int x012[o];
+    //concat points
+    int i = 0, j = 0, k = 0;
+    while(j < m)
+        x012[i++] = x01[j++];
+    while( k < n)
+        x012[i++] = x12[k++];
+
+    //find left and right
+    int* left, * right;
+    int middle = l/2;
+    if(x02[middle] < x012[middle]){
+        left = x02;
+        right = x012;
     }
-    //draw our last line
-    index1 = indices[indices_start];
-    index2 = indices[indices_start + stride - 1];
-    lineDraw(vert[index1], vert[index2], points, line_offset);
-    line_offset += lineLen(vert[index1], vert[index2]);
+    else{
+        left = x012;
+        right = x02;
+    }
 
-    //mergesort(points, *point_offset, line_offset);
-    //int total_y = points[line_offset].y - points[*(point_offset)].y;
-
-   
-    *point_offset = line_offset;
-    raster(points, start, *point_offset, point_offset);
+    //draw segments 
+    for(int i = p0.y; i < p2.y; i++){
+        for(int j = left[i - (int)p0.y]; j <= right[i - (int) p0.y]; j++){
+            if(i >= 0 && j >= 0 && i < HEIGHT && j < WIDTH)
+                frag[j][i] = p0.l;
+        }
+    }
 }
-
 
 
 
