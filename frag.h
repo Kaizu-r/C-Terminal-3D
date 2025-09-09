9ac9d260d279ed8cdd0d96da384b0248dd9eb7ca
Kaizu-r <jljoloc@gmail.com>
2025-09-10 01:28:33 +0800

    UPD> Changed fragments into a 1-D array for better memory layout

--- frag.h
+++ frag.h
@@ -15,45 +15,26 @@ typedef struct{
 } Frag;
 
 
-Frag ** makeFrag(int WIDTH, int HEIGHT){
-    Frag *values = (Frag*) calloc(WIDTH * HEIGHT, sizeof(Frag));
-    Frag ** frag = (Frag**) malloc(WIDTH * sizeof(Frag*));
-
-    for(int i = 0; i < WIDTH; i++){
-        frag[i] = values + i*HEIGHT;
-    }
-    
+Frag * makeFrag(int WIDTH, int HEIGHT){
+    Frag *frag = (Frag*) malloc(sizeof(Frag) * WIDTH * HEIGHT);  
     return frag;
 }
 
-void resetFrag(Frag** frag, int WIDTH, int HEIGHT){
-    for(int i = 0; i < WIDTH; i++){
-        for(int j = 0; j < HEIGHT; j++){
-            frag[i][j].flag = 0;
-        }
+int fragIndex(int x, int y, int WIDTH){
+    return x + y * WIDTH;
+}
+
+Frag getFrag(Frag* frag, int x, int y, int WIDTH){
+    return frag[fragIndex(x, y, WIDTH)];
+}
+
+void resetFrag(Frag* frag, int WIDTH, int HEIGHT){
+    for(int i = 0; i < WIDTH * HEIGHT; i++){
+        frag[i].flag = 0;
     }
 }
 
-//outdated
-void setFrag(vec3 points[], int point_len, float** frag, int WIDTH, int HEIGHT){
-    for(int i = 0; i < point_len; i++){
-        if(points[i].x >= 0 && points[i].x < WIDTH && points[i].y >= 0 && points[i].y < HEIGHT){
-            //frag[(int) points[i].x][(int) points[i].y] = points[i].l;
-        }
-    }
-}
 
-void fragTest(float** frag, int WIDTH, int HEIGHT){
-    float slope = 0.3/(HEIGHT);
-    float total = 0;
-    for(int i = 0; i < HEIGHT; i++){
-        for(int j = 0; j < WIDTH; j++){
-            frag[j][i] -= total;
-        }
-        //total = slope * i;
-        total = slope * i;
-    }
-}
 
 
 
