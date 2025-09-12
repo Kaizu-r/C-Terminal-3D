d9b415ee1e2126a84b16718b24c4c35d43053515
Kaizu-r <jljoloc@gmail.com>
2025-09-12 13:49:04 +0800

    UPD: Incorporated mesh to main

--- Main.c
+++ Main.c
@@ -5,6 +5,7 @@
 #include "renderer.h"
 #include "shader.h"
 #include "utils.h"
+#include "mesh.h"
 
 #define WIDTH 100
 #define HEIGHT 50
@@ -13,7 +14,7 @@
 int main(){
     
 
-    
+
     vec3 vertices[] = {
     //front
     0.0, 0.0, 0.0,
@@ -46,20 +47,26 @@ int main(){
         1, 6, 2 
     };
 
+    Mesh box;
+    box.vertices = vertices;
+    box.indices = indices;
+    
+
 
 
     int indCount1 = sizeof(indices)/sizeof(int);
 
 
-    //setup rotation here
+    //setup rotation h
     vec3 model_rotation = {0, 5, 0};
 
     //set up initial rotation
-    vec3 model_init_rotation = {45, 45, 45};
-
+    vec3 model_init_rotation = {5, 5, 5};
+    box.rotation = model_init_rotation;
 
     //setup translation here
     vec3 model_translation = {0, 0, 3.0};
+    box.position = model_translation;
 
     //setup camera here
     vec3 cam = {0, 0, 0};
@@ -73,7 +80,7 @@ int main(){
     //setup fov here
     float fov= 90;
 
-    vec3 total_rotation = model_init_rotation;
+    vec3 total_rotation = box.rotation;
     int n = sizeof(vertices)/sizeof(vec3);
     vec3 terminal[n];
     vec3 modVert[n];
@@ -90,16 +97,16 @@ int main(){
         
         //copy vertex data to modvert
         for(int i = 0; i < n; i++){
-            modVert[i] = vertices[i];
+            modVert[i] = box.vertices[i];
         }
 
         //edit if needed
-        total_rotation.x += model_rotation.x;
-        total_rotation.y += model_rotation.y;
-        total_rotation.z += model_rotation.z;
+        total_rotation.x += box.rotation.x;
+        total_rotation.y += box.rotation.y;
+        total_rotation.z += box.rotation.z;
 
 
-        modelTransform(modVert, n, 0.75, total_rotation,  model_translation);
+        modelTransform(modVert, n, 0.75, total_rotation,  box.position);
 
 
         for(int i = 0; i < n; i++){
@@ -109,9 +116,9 @@ int main(){
 
 
         int stride = 3;
-        int shapes = (sizeof(indices) / sizeof(int))/stride;
+        int shapes = indCount1/stride;
 
-        draw(terminal, indices, shapes, stride, near, far, fov, cam, frag, screen, WIDTH, HEIGHT);
+        draw(terminal, box.indices, shapes, stride, near, far, fov, cam, frag, screen, WIDTH, HEIGHT);
         
         
         
