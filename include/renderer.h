841f2677e4a059f573958914ae9e940101994dfb
Kaizu-r <jljoloc@gmail.com>
2025-12-27 00:52:08 +0800

    ADD: View matrix added to pipeline. ADD: Depth-based pixel values

--- include/renderer.h
+++ include/renderer.h
@@ -1,9 +1,10 @@
 #ifndef RENDERER_H
-#define RENDERER
+#define RENDERER_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+#include "camera.h"
 #include "coords.h"
 #include "utils.h"
 #include "vertex.h"
@@ -13,7 +14,7 @@
 
 float lightValue(vec3 col);
 char toAscii(float z);
-void draw(vec3 vertices[], int indices[], int shapes, int stride, float near, float far, float fov, vec3 cam, Frag * frag, char * screen, int WIDTH, int HEIGHT);
+void draw(vec3 vertices[], int indices[], int shapes, int stride, float near, float far, float fov, Camera cam, Frag * frag, char * screen, int WIDTH, int HEIGHT);
 void render(Frag *frag, int WIDTH, int HEIGHT, char screen[]);
 
 
@@ -36,15 +37,21 @@ char toAscii(float z){
 
 }
 
-void draw(vec3 vertices[], int indices[], int shapes, int stride, float near, float far, float fov, vec3 cam, Frag * frag, char * screen, int WIDTH, int HEIGHT){
+void draw(vec3 vertices[], int indices[], int shapes, int stride, float near, float far, float fov, Camera cam, Frag * frag, char * screen, int WIDTH, int HEIGHT){
         for(int i = 0; i < shapes; i++){
             tri tri1 = triangleBuild(vertices, indices, i);
 
             vec3 norm = normal(tri1.v1, tri1.v2, tri1.v3);
 
-            vec3 distance_to_cam = {tri1.v1.x - cam.x, tri1.v1.y - cam.y, tri1.v1.z - cam.z};
+            vec3 distance_to_cam = {tri1.v1.x - cam.position.x, tri1.v1.y - cam.position.y, tri1.v1.z - cam.position.z};
             if(dot(norm, distance_to_cam) < 0){
                 tri tri2 = tri1;
+                //move to camera space
+                view(&tri2.v1, 1, cam.position, cam.rotation);
+                view(&tri2.v2, 1, cam.position, cam.rotation);
+                view(&tri2.v3, 1, cam.position, cam.rotation);
+
+                //project the triangle vertices
                 proj(&tri2.v1, far, near, fov, WIDTH, HEIGHT);
                 proj(&tri2.v2, far, near, fov, WIDTH, HEIGHT);
                 proj(&tri2.v3, far, near, fov, WIDTH, HEIGHT);
@@ -79,7 +86,11 @@ void render(Frag *frag, int WIDTH, int HEIGHT, char screen[]){
         for(int j = 0; j < WIDTH; j++){
             if(frag[fragIndex( j, i, WIDTH, HEIGHT)].flag){
                 vec3 col = frag[fragIndex(j, i, WIDTH, HEIGHT)].color;
-                screen[k++] = toAscii((1 - (col.z / 4.0)));
+                // Map NDC z from [-1, 1] to [0, 1] for ASCII mapping
+                float depth = (col.z + 1.0f) * 0.5f;
+                if(depth < 0.0f) depth = 0.0f;
+                if(depth > 1.0f) depth = 1.0f;
+                screen[k++] = toAscii(depth);
             }
             else
                 screen[k++] = ' ';
